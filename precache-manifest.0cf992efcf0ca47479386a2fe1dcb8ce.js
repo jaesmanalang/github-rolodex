84a85cd7a365b97b506d8d542c0483909d5f3ec6
James Manalang <jaesmanalang@gmail.com>
2020-07-08 15:08:47 +0800

    Updates

---
+++ precache-manifest.0cf992efcf0ca47479386a2fe1dcb8ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06224c66c7f0800e2e93cb17e8d634e",
+    "url": "/github-rolodex/index.html"
+  },
+  {
+    "revision": "34f867f3ef17aca50833",
+    "url": "/github-rolodex/static/css/main.bb41f375.chunk.css"
+  },
+  {
+    "revision": "2fc16b8141a58e348a12",
+    "url": "/github-rolodex/static/js/2.d34a5b81.chunk.js"
+  },
+  {
+    "revision": "c5930055a21c439b1783da4e9887b5d2",
+    "url": "/github-rolodex/static/js/2.d34a5b81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34f867f3ef17aca50833",
+    "url": "/github-rolodex/static/js/main.d6bb695e.chunk.js"
+  },
+  {
+    "revision": "966c55962769b548ae87",
+    "url": "/github-rolodex/static/js/runtime-main.0cb1e76c.js"
+  },
+  {
+    "revision": "9536415991f2e7242bcdb58546293d56",
+    "url": "/github-rolodex/static/media/spinner.95364159.gif"
+  }
+]);
